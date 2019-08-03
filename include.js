a8b03a3695b26a71dddd5ad25e90bb9c071f22b1
marty <martysteer@gmail.com>
2019-08-03 10:51:50 +0100

    Initial commit of Gil's MythAxis.co.uk docroot

---
+++ include.js
@@ -0,0 +1,19 @@
+
+document.write("The include itself<P>");
+
+<!--document.write('<div class="sidebar"><img src="./toplist.jpg"<BR>')
+document.write('<A href="http://www.mythaxis.co.uk/index.htm"> Current Issue of Mythaxis</A><BR>')
+document.write('<img src="./divider.jpg"><BR>')
+document.write('<A href="http://www.mythaxis.co.uk/about.htm"> About Mythaxis</A><BR><BR>')
+document.write('<A href="http://www.mythaxis.co.uk/submission.htm"> Submissions</A><BR><BR>')
+document.write('<A href="http://www.mythaxis.co.uk/policy.htm"> Editorial Policy</A><BR>')
+document.write('<img src="./divider.jpg"><BR>')
+document.write('<A href="http://www.mythaxis.co.uk/forum/"> Forum</A><BR>')
+document.write('<img src="./divider.jpg"><BR>')
+document.write('<img src="./links.jpg"><BR>')
+document.write('<A href="http://www.dead-channel.net/" target = _blank>The Dead Channel - another web magazine</A><BR><BR>')
+document.write('<A href="http://www.carfilhiot.co.uk/saga/saga.htm" target = _blank>The Carfilhiot Saga - multi blog story</A><BR><BR>')
+document.write('<A href="http://www.amazonsystems.co.uk/pony/pp.htm" target = _blank>Travels in Middle Earth</A><BR><BR>')
+document.write('<A href="http://www.amazonsystems.co.uk/data/amazmuse.htm" target = _blank>More of Gil's enthusiasms</A><BR>')
+document.write('<img src="./endlist.jpg"><P>')
+document.write('</div>')-->
