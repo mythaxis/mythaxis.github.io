85e9a855da02640459c3fe64543aa9deac3fbf79
marty <martysteer@gmail.com>
2019-08-28 18:36:47 +0100

    added utf-8 <meta> to all html pages

--- webinstall.php
+++ webinstall.php
@@ -35,6 +35,7 @@ $start_time = time();
 echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
+<meta charset="UTF-8">
 		<title>', $txt['smf_installer'], '</title>
 		<style type="text/css">
 			body
@@ -195,6 +196,7 @@ function initialize_inputs()
 		echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 	<head>
+<meta charset="UTF-8">
 		<title>', $txt['ftp_path'], '</title>
 	<!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-146012578-1"></script>
